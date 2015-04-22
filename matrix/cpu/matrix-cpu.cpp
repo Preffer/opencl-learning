d4a4cc4455eb24cfcca2a465d67cc9e4fa65622a
Yuzo <hyzgog@gmail.com>
2015-04-22 21:10:13 +0800

    revise with 2 dim array

--- matrix/cpu/matrix-cpu.cpp
+++ matrix/cpu/matrix-cpu.cpp
@@ -12,23 +12,32 @@ int main(int argc, char *argv[]) {
 
 	const int R = stoi(argv[1]);
 
-	float* A = new float[R * R];
-	float* B = new float[R * R];
-	float* C = new float[R * R];
+	float** A = new float*[R];
+	float** B = new float*[R];
+	float** C = new float*[R];
 
-	for (int i = 0; i < R * R; i++) {
-		A[i] = rand() % 100;
-		B[i] = rand() % 100;
+	for (int row = 0; row < R; row++) {
+		A[row] = new float[R];
+		B[row] = new float[R];
+		C[row] = new float[R];
+		float* a = A[row];
+		float* b = B[row];
+		for (int col = 0; col < R; col++) {
+			a[col] = rand() % 100;
+			b[col] = rand() % 100;
+		}
 	}
 
 	#pragma omp parallel for
 	for (int row = 0; row < R; row++) {
+		float* a = A[row];
 		for(int col = 0; col < R; col++) {
+			float* b = B[col];
 			float sum = 0;
 			for (int i = 0; i < R; i++) {
-				sum += A[row * R + i] * B[i * R + col];
+				sum += a[i] * b[i];
 			}
-			C[row * R + col] = sum;
+			C[row][col] = sum;
 		}
 	}
 
