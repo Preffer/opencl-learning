8e7ef329de474eadff5a72f20dded273fd54430e
PillowSky <hyzgog@gmail.com>
2015-04-22 21:43:29 +0800

    update output method

--- matrix/cpu/matrix-cpu.cpp
+++ matrix/cpu/matrix-cpu.cpp
@@ -3,10 +3,9 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-	// Create the two input vectors and one result matrix
+int main(int argc, char* argv[]) {
 	if (argc < 2) {
-		cout << "Usage: ./matrix (R)" << endl;
+		cout << "Usage: ./matrix R [save](default: no)" << endl;
 		return 2;
 	}
 
@@ -31,7 +30,7 @@ int main(int argc, char *argv[]) {
 	#pragma omp parallel for
 	for (int row = 0; row < R; row++) {
 		float* a = A[row];
-		for(int col = 0; col < R; col++) {
+		for (int col = 0; col < R; col++) {
 			float* b = B[col];
 			float sum = 0;
 			for (int i = 0; i < R; i++) {
@@ -41,15 +40,15 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	if(argc == 2){
+	if (argc > 2) {
 		ofstream outa("a.txt");
 		ofstream outb("b.txt");
 		ofstream outc("c.txt");
 		for (int row = 0; row < R; row++) {
-			for (int col = 0; col < R; col ++) {
-				outa << A[row * R + col] << " ";
-				outb << B[row * R + col] << " ";
-				outc << C[row * R + col] << " ";
+			for (int col = 0; col < R; col++) {
+				outa << A[row][col] << " ";
+				outb << B[row][col] << " ";
+				outc << C[row][col] << " ";
 			}
 			outa << endl;
 			outb << endl;
